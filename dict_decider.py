24c9675980074e48c7c57bd0a66688bf7a932d5f
Quinton Miller <qjm412@gmail.com>
2023-12-16 19:39:51 -0500

    added lists and some format changes

--- dict_decider.py
+++ dict_decider.py
@@ -1,8 +1,14 @@
 import random as r
 
 full_dict = {
-    "sub1" : ["1","2","3","4","5","6","7","8","9","10","11","12","13","14"],
-    "sub2" : ["1","2","3","4","5","6","7","8","9","10","11","12","13","14"]
+    "Thighs" : ["girl on her knees","fishnets","skirt","face",
+                "pair of thigh highs","selfie","\"sfw\" image","exposed midriff",
+                "exposed panties","cleavage","naked ass","fully nude girl",
+                "pussy","butt cheek","exposed nipple","solo image","thigh"],
+    "thighdeology" : ["thigh/butt being squeezed","topless girl","cameltoe",
+                      "pair of thigh highs","garter","skirt","exposed panties",
+                      "exposed midriff","\"sfw\" image","exposed boobs","bikini",
+                      "ass","solo image","thigh"]
 }
 
 def roll(sub):
@@ -37,13 +43,13 @@ def z_mods(x,y,z,score):
             mod_x = 14
             mod_y = 14
         elif z_2 <= 9:
-            if score >= 1000:
+            if score >= 100:
                 end = True
             else:
                 mod_x = 14
                 mod_y = 14
         elif z_2 <= 10:
-            if score >= 500:
+            if score >= 50:
                 end = True
             else:               # bad ending
                 mod_x = 15 
@@ -64,9 +70,9 @@ def x_num(x):
     
 def y_trig(y,sub):
     if y <= len(full_dict[sub]):
-        trig =  full_dict[sub][y-1]
+        trig = "\033[93m" + full_dict[sub][y-1] + "\033[0m"
     elif y <= len(full_dict[sub])+3:
-        trig = full_dict[sub][len(full_dict[sub])-(y-len(full_dict[sub]))] + " and " + full_dict[sub][r.randint(y-len(full_dict[sub]),len(full_dict[sub])-2)]
+        trig = "\033[93m" + full_dict[sub][len(full_dict[sub])-(y-len(full_dict[sub]))] + "\033[0m" + " and each " + "\033[93m" + full_dict[sub][r.randint(y-len(full_dict[sub]),len(full_dict[sub])-2)] + "\033[0m"
     return trig
 
 def main():
@@ -75,20 +81,27 @@ def main():
     while end == False:
         sub = list(full_dict.keys())[r.randrange(0,len(full_dict))]
         x, y, z = roll(sub)
-        score += (x * y) + z
         extra_x = False
         x, y, end = z_mods(x,y,z,score)
         num, extra_x = x_num(x)
+        round_score = 1 + int(num * (y / len(full_dict[sub])))//20
+        score += round_score
         trig = y_trig(y,sub)
         print()
-        print("Go to https://www.reddit.com/r/" + sub + " .")
-        print("Look at the next",num,"posts.")
-        print("Edge every time you see " + trig + ".")
+        print("Go to https://www.reddit.com/r/" + "\033[94m" + sub + "\033[0m")
+        print("Look at the next" + "\033[91m" , num , "\033[0m" + "posts.")
+        print("Edge once for each " + trig + ".")
+        print("Difficulty: " + str(round_score) + "/10",end="")
+        if round_score > 10:
+            print(", good luck!")
+        else:
+            print("")
         if extra_x == True:
             print("Every time you edge add 1 more post to the total amount.")
         if end == False:
             input("Press enter to continue.")
         else:
             input("You are free, press enter to end.") 
+            print("Final score:",score)
 
 main()
